5b3235eb70a1ae2beb66376611b9cc0e80c1b1e3
Nico Scha <64054331+jesuisnicolas@users.noreply.github.com>
2021-04-25 00:37:45 -0300

    Week 1 Assignments

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const links = [
+    {
+        label: "Week 1 Assignments",
+        url: "w01/index.html"
+    },
+    // {
+    //     label: "Week 2 Assignments",
+    //     url: "w02/index.html"
+    // }
+  ]
+
+
+function createListOfLinks(){
+
+    // let ol = document.getElementById("assignmentsList");
+    
+    links.forEach(link => {
+        var li = document.createElement("li");
+        
+        li.setAttribute('class', "assignmentItem");
+
+        // document.getElementById('assignmentsList').appendChild(li);
+        document.querySelector("ol.assignmentsList").appendChild(li);
+
+        var a = document.createElement("a");
+        a.setAttribute('class', "assignmentLink");
+        a.setAttribute('href', link.url.toString());
+        a.innerHTML = link.label.toString();
+        li.appendChild(a);
+});
+
+}
+
+createListOfLinks();
