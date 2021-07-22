e9a53d00d433d647e8a82b7998a0a542ae3cea3e
Nico Scha <64054331+jesuisnicolas@users.noreply.github.com>
2021-07-21 23:44:30 -0300

    some polish

--- js/main.js
+++ js/main.js
@@ -40,10 +40,6 @@ const links = [
         url: "w10/index.html"
     },
     {
-        label: "Week 11 Assignments",
-        url: "w11/index.html"
-    },
-    {
         label: "Final Project",
         url: "final-project/index.html"
     }
